e1b4d3279105db4f39387e470ee35156be9c66ee
cruz-IS <edwarcruz2000@yahoo.com>
2025-03-04 23:41:15 -0600

    se agrego la ruta de login y signup

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'campusvirtual.unah.edu.hn',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
